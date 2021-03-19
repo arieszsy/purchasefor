c4539662d874a73343a32d02f991f44f9eeafe78
arieszsy <63033840+arieszsy@users.noreply.github.com>
2021-03-19 19:54:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,11 +21,7 @@ pd.set_option('display.max_columns', None)
 Filepath="E:\\gittz\\purchasefor\\files\\train_set.csv"
 df = pd.read_csv(Filepath, header=0)
 
-# 查看样本分布是否不平衡
-x = df.iloc[:, :-1] #切片，得到输入x
-y = df.iloc[:, -1] #切片，得到标签y
-groupby_data_orgianl = df.groupby('y')['y'].count() #对label做分类汇总
-print (groupby_data_orgianl) #打印输出原始数据集样本分类分布
+
 # 分类变量转码
 encoder = LabelEncoder()
 labels=['job','marital','education','default','housing','loan','contact','poutcome']
@@ -33,9 +29,27 @@ for label in labels:
     df[label] = encoder.fit_transform(df[label].values)
 df['dd']='2014'+df['month']+df['day'].map(str)
 df['dt']=df['dd'].apply(lambda x:datetime.datetime.strptime(x,"%Y%b%d"))
-df=df.drop(['dd','day','month','ID'],axis=1)
+
+df['dif_day']=(max(df['dt'])-df['dt']).dt.days
 df.insert(df.shape[1]-1,'y',df.pop('y'))
-print(df)
+df=df.drop(['dd','day','month','ID','dt'],axis=1)
+# print(df)
+# 查看样本分布是否不平衡
+x = df.iloc[:, :-1] #切片，得到输入x
+y = df.iloc[:, -1] #切片，得到标签y
+# groupby_data_orgianl = df.groupby('y')['y'].count() #对label做分类汇总
+# print (groupby_data_orgianl) #打印输出原始数据集样本分类分布
+
+#使用SMOTE方法进行过抽样处理
+model_smotenc = SMOTENC(random_state=40,categorical_features=[1,2,3,4,6,7,8,13]) #建立SMOTE模型对象
+x_smote_resampled, y_smote_resampled = model_smotenc.fit_resample(x,y) #输入数据并作过抽样处理
+# 遍历所有列名，排除不需要的
+# cols = [i for i in df.columns if i not in ['y']]
+# x_smote_resampled = df[cols]
+# print(x_smote_resampled)
+print(y_smote_resampled)
+print (y_smote_resampled.sum()/y_smote_resampled.count()) #打印输出原始数据集样本分类分布
+
 
 
 
